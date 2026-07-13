efaaba7802b648672d55d31209c493c01ab8ecd1
Documenter.jl <documenter@juliadocs.github.io>
2026-07-13 20:29:51 +0000

    build based on 266b819

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#Acqua-Calda","page":"Home","text":"This is for the setup of the workflow and development. This will be added/finished over time","title":"Acqua Calda"}]
+}
